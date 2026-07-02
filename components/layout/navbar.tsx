8fe749e8e9b1b9f880b24c65f4550751524d6afd
Shek Rasel <swe.rasel@gmail.com>
2026-07-02 14:43:21 +0600

    navbar done

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,8 +1,16 @@
+import { navigationItems } from "@/constant/navigation";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-white text-black py-3 px-6 shadow-sm flex gap-8 rounded-full mt-6">
+      {navigationItems.map((item) => (
+        <Link href={item.href} key={item.id} className="hover:text-blue-500">
+          {item.label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
